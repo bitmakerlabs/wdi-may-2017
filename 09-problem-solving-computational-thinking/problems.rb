b942e6b1906d02ef109ecd0ce41d8c883633b1b0
NatalieBlack <natalievictoriablack@gmail.com>
2017-05-17 23:20:03 -0400

    add problems for 09-problem-solving-computational-thinking

---
+++ 09-problem-solving-computational-thinking/problems.rb
@@ -0,0 +1,16 @@
+# Problem 1: get the total sum of a list of numbers
+
+
+# Problem 2:
+# Fizzbuzz: for every number between 1 and 100:
+# print "fizz" for each number that is divisible by 3,
+# print "buzz" for each number that is divisible by of 5,
+# print "fizzbuzz" for each number that is divisible by both 3 and 5
+# for all other numbers print the number itself
+
+
+# Problem 3: find frequency of the letter "s" in a string
+
+# Problem 4: find frequency of the letter "a" in a string
+
+# Problem 5: find the most frequent letter in a string
