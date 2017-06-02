ead96fd8f6125faf4dcd567a92b7b1e953ab169c
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-06-02 11:30:27 -0400

    Add Recipe model

--- 19-web-frameworks-part-2/cookbook/server.rb
+++ 19-web-frameworks-part-2/cookbook/server.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require_relative 'recipe'
 
 get '/' do
   redirect to('/recipes')
@@ -17,13 +18,10 @@ get '/blog' do
 end
 
 get '/recipes' do
-
-  @recipes = [
-    'BBQ Ribs',
-    'Avocado Sushi',
-    'Gluten Free Pancakes',
-    'Slow cooker stew'
-  ]
-
+  @recipes = Recipe.all
   erb :recipes
+end
+
+get '/recipes/:id' do
+  params[:id]
 end
