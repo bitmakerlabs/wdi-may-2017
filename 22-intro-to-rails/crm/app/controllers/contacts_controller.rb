269cbbc14546f6218034f5bf19164e3d20d72da5
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-06-07 12:08:18 -0400

    Implement Contact#create

--- 22-intro-to-rails/crm/app/controllers/contacts_controller.rb
+++ 22-intro-to-rails/crm/app/controllers/contacts_controller.rb
@@ -8,8 +8,17 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.create(contact_params)
+    redirect_to contact_url(@contact)
   end
 
   def show
   end
+
+private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :notes)
+  end
+
 end
