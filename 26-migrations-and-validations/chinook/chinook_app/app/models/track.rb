f406a232a0f839fc9f025fa35b6a7216d52fac2c
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-06-13 12:08:13 -0400

    Add validations

--- 26-migrations-and-validations/chinook/chinook_app/app/models/track.rb
+++ 26-migrations-and-validations/chinook/chinook_app/app/models/track.rb
@@ -9,12 +9,32 @@ class Track < ApplicationRecord
   scope :medium, -> { longer_than(SHORT).shorter_than(LONG) }
   scope :long,   -> { longer_than(LONG) }
 
+  validates :name, :composer, :milliseconds, :bytes, :unit_price, presence: true
+  validates :milliseconds, numericality: { greater_than: 0 }
+  validates :unit_price,   numericality: { greater_than_or_equal_to: 0.0 }
+
+  validate :name_must_be_titleized
+
   def self.shorter_than(milliseconds)
     where('milliseconds < ?', milliseconds)
   end
 
   def self.longer_than(milliseconds)
     where('milliseconds >= ?', milliseconds)
+  end
+
+private
+
+  def name_must_be_titleized
+
+    # Make sure first char is upper cased
+    first_char = name[0]
+    first_char_is_not_upcased = (first_char != first_char.upcase)
+
+    # if first charater is not upper cased, add an error
+    if first_char_is_not_upcased
+      errors.add(:name, 'must be capitalized!')
+    end
   end
 
 end
