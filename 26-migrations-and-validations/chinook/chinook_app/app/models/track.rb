257ce8494a21556e32723b00e8afeeb2d78d4d9e
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-06-13 10:25:26 -0400

    Add scope class methods to Track

--- 26-migrations-and-validations/chinook/chinook_app/app/models/track.rb
+++ 26-migrations-and-validations/chinook/chinook_app/app/models/track.rb
@@ -1,2 +1,26 @@
 class Track < ApplicationRecord
+
+  SHORT = 180000
+  LONG  = 360000
+
+  def self.short
+    shorter_than(SHORT)
+  end
+
+  def self.medium
+    longer_than(SHORT).shorter_than(LONG)
+  end
+
+  def self.long
+    longer_than(LONG)
+  end
+
+  def self.shorter_than(milliseconds)
+    where('milliseconds < ?', milliseconds)
+  end
+
+  def self.longer_than(milliseconds)
+    where('milliseconds >= ?', milliseconds)
+  end
+
 end
