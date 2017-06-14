8572363b473aaff6fa0daa46228b87cb82991513
NatalieBlack <natalievictoriablack@gmail.com>
2017-06-14 10:56:15 -0400

    add comment model to photogur

---
+++ 27-associations/photogur/app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+  # there is a picture_id foreign key in your database table
+  belongs_to :picture
+  # @comment.picture
+  # @comment.picture= @picture
+  # @comment.build_picture
+  # @comment.create_picture
+end
--- 27-associations/photogur/app/models/picture.rb
+++ 27-associations/photogur/app/models/picture.rb
@@ -1,2 +1,7 @@
 class Picture < ActiveRecord::Base
+  # there is a picture_id foreign key referring to you in the comments table
+  has_many :comments
+  # @picture.comments
+  # @picture.comments <<
+  # etc
 end
