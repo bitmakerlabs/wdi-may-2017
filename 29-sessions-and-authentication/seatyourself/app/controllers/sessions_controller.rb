4962ecf8398857a068a7d1aa24d7c833fccc0407
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-06-16 12:05:56 -0400

    Implement login

--- 29-sessions-and-authentication/seatyourself/app/controllers/sessions_controller.rb
+++ 29-sessions-and-authentication/seatyourself/app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    u = User.find_by(email: params[:session][:email])
+
+    if u && u.authenticate(params[:session][:password])
+      flash[:notice] = 'Successfully logged in!'
+      redirect_to root_url
+    else
+      flash.now[:alert] = 'Try again!'
+      render :new
+    end
+
   end
 
   def destroy
