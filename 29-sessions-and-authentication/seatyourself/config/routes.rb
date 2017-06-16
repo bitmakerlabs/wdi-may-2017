15b433fabc5a378f017fcc34b30ac9a1adb589bd
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-06-16 11:58:37 -0400

    Remove useless session views, change sessions routes to restful routes

--- 29-sessions-and-authentication/seatyourself/config/routes.rb
+++ 29-sessions-and-authentication/seatyourself/config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   get 'home/show'
   root 'home#show'
 
   resource :users, only: %i(new create)
+  resource :sessions, only: %i(new create destroy)
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
