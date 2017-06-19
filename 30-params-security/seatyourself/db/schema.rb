3170ffa75b52672a7d939fe9dfcc313136f7d1bc
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-06-19 11:40:31 -0400

    Introduce mass assignment vulnerability with credits

--- 30-params-security/seatyourself/db/schema.rb
+++ 30-params-security/seatyourself/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619152418) do
+ActiveRecord::Schema.define(version: 20170619153350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170619152418) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "bio"
+    t.integer "credits"
   end
 
 end
