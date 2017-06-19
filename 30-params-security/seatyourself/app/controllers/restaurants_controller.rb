d836157beba7f4f7ceb8ead641cfe6a3507e8977
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-06-19 11:05:35 -0400

    Implement Restaurants#show

--- 30-params-security/seatyourself/app/controllers/restaurants_controller.rb
+++ 30-params-security/seatyourself/app/controllers/restaurants_controller.rb
@@ -3,5 +3,16 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+
+    Rails.logger.info "*** params[:controller] = #{ params[:controller] }"
+    Rails.logger.info "*** params[:action] = #{ params[:action] }"
+    Rails.logger.info "*** params[:id] = #{ params[:id] }"
+    Rails.logger.info "*** params[:lang] = #{ params[:lang] }"
+
+    @lang = params[:lang]
+    @country = params[:country]
+    @restaurant = Restaurant.find(params[:id])
   end
 end
+
+# Querystring
--- 30-params-security/seatyourself/config/routes.rb
+++ 30-params-security/seatyourself/config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-
-  get 'restaurants/show'
-
   get 'home/show'
   root 'home#show'
 
   resource :users, only: %i(new create)
   resource :sessions, only: %i(new create destroy)
+
+  resources :restaurants, only: %i(index show)
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
