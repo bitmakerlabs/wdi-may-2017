6461bfcb394c0aa45b4d464895e76c3d6a2a599e
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-06-19 10:15:04 -0400

    Generate restaurant model with: bin/rails generate model Restaurant name:string description:text --no-test-framework

--- 30-params-security/seatyourself/db/schema.rb
+++ 30-params-security/seatyourself/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616144626) do
+ActiveRecord::Schema.define(version: 20170619141423) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
