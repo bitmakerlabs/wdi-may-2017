6984ccc98cd7a5a2da1384a8f3f2b7e4d2c3ae66
NatalieBlack <natalievictoriablack@gmail.com>
2017-06-20 20:53:22 -0400

    add test cases

--- 32-how-to-test/seatyourself/test/models/restaurant_test.rb
+++ 32-how-to-test/seatyourself/test/models/restaurant_test.rb
@@ -8,13 +8,25 @@ class RestaurantTest < ActiveSupport::TestCase
   def test_restaurant_is_available_when_under_capacity_during_opening_hours
   end
 
+  def test_restaurant_is_available_later_same_day
+  end
+
+  def test_restaurant_is_unavailable_in_the_past
+  end
+
   def test_restaurant_is_unavailable_when_full
   end
 
   def test_restaurant_is_unavailable_before_opening
   end
 
-  def test_restaurant_is_unavailable_half_an_hour_before_closing
+  def test_restaurant_is_available_at_opening
   end
-  
+
+  def test_restaurant_is_unavailable_at_closing_time
+  end
+
+  def test_restaurant_is_unavailable_past_closing_time
+  end
+
 end
