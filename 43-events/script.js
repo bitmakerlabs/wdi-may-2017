e8d1e6b26b91831044723249fea020ec5b8a0fa3
Mina Mikhail <mina@fightingtheboss.com>
2017-07-06 12:52:07 -0400

    Add in-class code for 43 JS Events

--- 43-events/script.js
+++ 43-events/script.js
@@ -1,11 +1,40 @@
-$(document).on('ready', function() {
+document.addEventListener('DOMContentLoaded', function() {
+  // All of our DOM manipulation code goes in here
 
+  var link = document.querySelector('#click-bait');
+
+  function callback(eventObject) {
+    eventObject.preventDefault();
+    eventObject.stopPropagation();
+    window.alert('#click-bait was clicked!');
+  }
+
+  link.addEventListener('click', callback);
+
+  var secondLevel = document.querySelector('#second-level');
+
+  secondLevel.addEventListener('click', function(e) {
+    e.stopPropagation();
+    window.alert('#second-level received click event!');
+  });
+
+  var firstLevel = document.querySelector('#first-level');
+
+  var firstLevelEventHandler = function() {
+    window.alert('#first-level received click event!');
+  };
+
+  firstLevel.addEventListener('click', firstLevelEventHandler);
+
+  window.addEventListener('keyup', function(e) {
+    var keypressed = document.querySelector('.keypressed');
+    keypressed.innerHTML = e.which;
+
+    switch (e.which) {
+      case 32:
+        console.log('Space bar was pressed');
+        break;
+      default:
+    }
+  });
 });
-
-// $(function() {
-//
-// });
-
-// $(document).ready(function() {
-//
-// });
